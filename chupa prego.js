febefabea603ac697d3d03f60a3c4cfd09b22945
João Pedro G U <41847866+Charmandercharmandesco@users.noreply.github.com>
2024-05-09 20:55:46 -0300

    memes

---
+++ chupa prego.js
@@ -0,0 +1,12 @@
+//O vento DOM é acionado quando todo o HTML foi completamente carregado
+document.addEventListener('DOMContentLoaded', function() {
+    alert(123);
+    //Tipos de dados
+    //String: Sequência de caracteres
+    //Number: números inteiros ou pontos flutuantes
+    //Boolean: retorna dois valores, true ou false
+    //object: estrutura de dados que pode armazenar múltiplos valores
+    //NULL: quando está vazio;
+    //array: objeto especial ordenado;
+    //function: bloco de código reutilizável;
+})

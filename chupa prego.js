783c69c456281dedb77cc1e2aae0e4832711ee15
João Pedro G U <41847866+Charmandercharmandesco@users.noreply.github.com>
2024-05-09 21:38:53 -0300

    Commit da nota para degolamento

--- chupa prego.js
+++ chupa prego.js
@@ -1,6 +1,5 @@
 //O vento DOM é acionado quando todo o HTML foi completamente carregado
 document.addEventListener('DOMContentLoaded', function() {
-    alert(123);
     //Tipos de dados
     //String: Sequência de caracteres
     //Number: números inteiros ou pontos flutuantes
@@ -9,4 +8,24 @@ document.addEventListener('DOMContentLoaded', function() {
     //NULL: quando está vazio;
     //array: objeto especial ordenado;
     //function: bloco de código reutilizável;
+
+    //CONST É usado para declarar uma variável cujo valor é permanente
+    const teste = 50
+
+    //LET é usado para declarar variáveis que os valores podem ser retribuidos
+    let C = 0
+    C = 0
+    console.log(C)
+    
+    const notaMedia = 5;
+    
+    if(notaMedia >= 6) {        
+        alert("passou"); // Ele será liberto 😒😒😒😒😒
+    }
+    else if (notaMedia > 4 && notaMedia < 6) {
+        alert("foi quase mane"); // 🤢🤢🤢🤢🤢
+    }
+    else {
+        alert("reprovado"); // O homem será degolado 😂😂😂😂😂
+    }
 })

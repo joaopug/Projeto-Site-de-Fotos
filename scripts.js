68df4b66097f1111eebe902b8e36ebab34acc47d
João Pedro G U <41847866+Charmandercharmandesco@users.noreply.github.com>
2024-05-16 22:31:42 -0300

    bkmn

--- scripts.js
+++ scripts.js
@@ -1,146 +1,20 @@
 document.addEventListener("DOMContentLoaded", function (){
 
-alert(123);
+    const baseImagens = {
+        todes: ['imagem.jpg', 'city.jpg', 'animal.jpg', 'natureza.jpg'],
+        nature: [],
+        cidade: [],
+        animal: [],
+    }
 
+    // Evento de clique
+    document.body.addEventListener('click', function (event){
+        //console.log(event);
+        if(event.target.classList.contains('botao-categoria')); // Acessa a lista de classe de onde clicou e procura pela classe botao
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    })
 
 
 
